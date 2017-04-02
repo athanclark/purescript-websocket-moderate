6fe54b4e0c68884eabac9902377dfd645f39fffb
Athan Clark <athan.clark@gmail.com>
2017-04-02 03:30:15 -0600

    initial commit

---
+++ src/WebSocket.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var platformSpecificWS = typeof module !== "undefined" && module.require
+      ? module.require('ws')
+      : WebSocket;
+
+
+exports.newWebSocketImpl = function runNewWebSocketImpl (params) {
+  console.log("starting");
+  var socket = new platformSpecificWS(params.url, params.protocols);
+  var cont = params.continue({ url : socket.url, protocol : socket.protocol });
+  var capabilities = {
+    "send"              : function(m) {socket.send(m);},
+    "close"             : function() {socket.close();},
+    "close\'"           : function(xs) {socket.close(xs.code,xs.reason);},
+    "getBufferedAmount" : function() {return socket.bufferedAmount;}
+  };
+
+
+  socket.addEventListener("close", function(e) {
+    cont.onclose({
+      code: e.code,
+      reason: e.reason,
+      wasClean: e.wasClean
+    });
+  });
+
+  socket.addEventListener("error", function(e) {
+    cont.onerror(JSON.stringify(e));
+  });
+
+  socket.addEventListener("message", function(e) {
+    cont.onmessage(capabilities, e.data);
+  });
+
+  socket.addEventListener("open", function() {
+    cont.onopen(capabilities);
+  });
+
+  console.log("finished");
+};
---
+++ src/WebSocket.purs
@@ -0,0 +1,98 @@
+module WebSocket where
+
+import Prelude
+import Data.Function.Eff (EffFn1, EffFn2, runEffFn1, mkEffFn1, mkEffFn2)
+import Data.Nullable (Nullable, toMaybe, toNullable)
+import Data.Maybe (Maybe)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Console (CONSOLE,log)
+
+
+
+foreign import data WEBSOCKET :: !
+
+
+type Capabilities eff =
+  { send              :: String -> Eff eff Unit
+  , close             :: Eff eff Unit
+  , close'            :: { code :: Maybe Int, reason :: Maybe String } -> Eff eff Unit
+  , getBufferedAmount :: Eff eff Int
+  }
+
+type Environment =
+  { url      :: String
+  , protocol :: String
+  }
+
+type Params eff =
+  { url       :: String
+  , protocols :: Array String
+  , continue  :: Environment ->
+      { onclose   :: { code     :: Int
+                     , reason   :: Maybe String
+                     , wasClean :: Boolean
+                     } -> Eff eff Unit
+      , onerror   :: String                     -> Eff eff Unit
+      , onmessage :: Capabilities eff -> String -> Eff eff Unit
+      , onopen    :: Capabilities eff -> Eff eff Unit
+      }
+  }
+
+
+newWebSocket :: forall eff
+              . Params (ws :: WEBSOCKET | eff)
+             -> Eff (err :: EXCEPTION, ws :: WEBSOCKET | eff) Unit
+newWebSocket params =
+  runEffFn1 newWebSocketImpl
+    { url: params.url
+    , protocols: params.protocols
+    , continue: \env ->
+        let conts = params.continue env
+        in  { onclose: mkEffFn1 $ \{code,reason,wasClean} -> conts.onclose
+                {code, reason: toMaybe reason, wasClean}
+            , onerror: mkEffFn1 conts.onerror
+            , onmessage: mkEffFn2 (conts.onmessage <<< runCapabilitiesImpl)
+            , onopen: mkEffFn1 (conts.onopen <<< runCapabilitiesImpl)
+            }
+    }
+  where
+    runCapabilitiesImpl :: forall eff. CapabilitiesImpl eff -> Capabilities eff
+    runCapabilitiesImpl cs =
+      { send: runEffFn1 cs.send
+      , close: cs.close
+      , close': \{code,reason} -> runEffFn1 cs.close' {code: toNullable code, reason: toNullable reason}
+      , getBufferedAmount: cs.getBufferedAmount
+      }
+
+
+
+-- * Impl
+
+type CapabilitiesImpl eff =
+  { send              :: EffFn1 eff String Unit
+  , close             :: Eff eff Unit
+  , close'            :: EffFn1 eff { code :: Nullable Int, reason :: Nullable String } Unit
+  , getBufferedAmount :: Eff eff Int
+  }
+
+type ParamsImpl eff =
+  { url       :: String
+  , protocols :: Array String
+  , continue  :: Environment ->
+      { onclose   :: EffFn1 eff { code     :: Int
+                                , reason   :: Nullable String
+                                , wasClean :: Boolean
+                                } Unit
+      , onerror   :: EffFn1 eff String Unit
+      , onmessage :: EffFn2 eff (CapabilitiesImpl eff) String Unit
+      , onopen    :: EffFn1 eff (CapabilitiesImpl eff) Unit
+      }
+  }
+
+
+
+foreign import newWebSocketImpl :: forall eff
+                                . EffFn1 (err :: EXCEPTION, ws :: WEBSOCKET | eff)
+                                    (ParamsImpl (ws :: WEBSOCKET | eff))
+                                    Unit
