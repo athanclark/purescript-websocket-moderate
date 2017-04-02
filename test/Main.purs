00c1f09bf4320cc39f3fc8afac92122deec82a5b
Athan Clark <athan.clark@gmail.com>
2017-04-02 03:39:53 -0600

    wait... it actually works :|

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,26 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Console (CONSOLE, log, error)
+import WebSocket (WEBSOCKET, newWebSocket)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Eff (console :: CONSOLE, ws :: WEBSOCKET, err :: EXCEPTION | e) Unit
 main = do
-  log "You should add some tests."
+  log "mer"
+  newWebSocket
+    { url: "ws://echo.websocket.org"
+    , protocols: []
+    , continue: \env ->
+        { onclose: \{code,reason,wasClean} -> do
+            log $ "code: " <> show code
+            log $ "reason: " <> show reason
+            log $ "was clean: " <> show wasClean
+        , onerror: \e -> error $ "error: " <> e
+        , onmessage: \_ m -> do
+            log $ "message: " <> m
+        , onopen: \{send} -> do
+            log "open"
+            send "yo"
+        }
+    }
